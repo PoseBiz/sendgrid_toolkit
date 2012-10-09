1a6b92fbc719bf60d4f66b85300626da8aae383a
Andrew Cholakian <andrew@andrewvc.com>
2012-10-09 11:40:56 -0700

    More newsletter stuff

--- lib/sendgrid_toolkit/newsletter.rb
+++ lib/sendgrid_toolkit/newsletter.rb
@@ -9,17 +9,29 @@ module SendgridToolkit
       def get(options={})
         api_req(:get, 'newsletter/lists/get',options).to_a
       end
-
+      
       def add(options={})
         api_req(:post, 'newsletter/lists/add',options)
       end
-
+      
       def edit(options={})
         api_req(:post, 'newsletter/lists/edit',options)
       end
-
+      
       def delete(options={})
         api_req(:post, 'newsletter/lists/delete',options)
+      end
+      
+      class Email < AbstractSendgridClient
+        def add(options={})
+          api_req(:post, 'newsletter/lists/email/add', options)
+        end
+        def get(options={})
+          api_req(:get, 'newsletter/lists/email/get', options)
+        end
+        def delete(options={})
+          api_req(:delete, 'newsletter/lists/email/delete', options)
+        end
       end
     end
   end
